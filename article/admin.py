810cb2155e3da9f59a1ebe4db80f429fa5b4bf00
chenyongzhe <1276316543@qq.com>
2019-04-19 18:05:22 +0800

    结束

--- article/admin.py
+++ article/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Article,ArticleUserinfor,Video,Type,Message
-# Register your models here.
+from .models import Article,ArticleUserinfor,Video,Type,Message,Videolist,Vcomment,Message,IpTable,Danmu,Comment,Usermessage
+admin.site.site_header = '伊甸园的烦恼后台管理'
+admin.site.site_title = '伊甸园的烦恼后台管理'
+admin.site.index_title="管理网站数据"
+
 admin.site.register(Article)
 admin.site.register(ArticleUserinfor)
-admin.site.register(Video)
-admin.site.register(Type)
+admin.site.register(Videolist)
 admin.site.register(Message)
+admin.site.register(Vcomment)
+admin.site.register(Comment)
+admin.site.register(Danmu)
+admin.site.register(Usermessage)
+admin.site.register(IpTable)
